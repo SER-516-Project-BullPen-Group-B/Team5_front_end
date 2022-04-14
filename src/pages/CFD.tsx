9d2d453fb7ca1099c96a1a56a13a5b5804277bda
kskd1804 <kskd1804@gmail.com>
2022-04-14 13:30:11 -0700

    US17 - Added Chart for CFD

--- src/pages/CFD.tsx
+++ src/pages/CFD.tsx
@@ -1,5 +1,62 @@
 import Nav from '../components/NavBar';
 import DropDown from '../components/DropDown';
+import { 
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    LineElement, 
+    Filler
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement, Filler);
+
+const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Work in Progress",
+      },
+    },
+    elements: {
+        line: {
+            tension: 0.4
+        }
+    }
+  };
+
+const data = {
+    labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+    datasets: [
+        {
+            label: "To Do",
+            data: [33, 53, 85, 41, 44, 65, 33, 54, 23, 87],
+            backgroundColor: "rgba(51, 181, 229, 0.4)",
+            borderColor: "rgba(51, 181, 229, 0.9)",
+            fill: true,
+        },
+        {
+            label: "In Progress",
+            data: [65, 33, 54, 23, 87, 33, 53, 85, 41, 44],
+            fill: true,
+            backgroundColor: "rgba(255, 187, 51, 0.4)",
+            borderColor: "rgba(255, 187, 51, 0.9)"
+        },
+        {
+            label: "Done",
+            data: [23, 87, 33, 53, 85, 41, 44, 65, 33, 54],
+            fill: true,
+            backgroundColor: "rgba(0,200,81,0.4)",
+            borderColor: "rgba(0,200,81,0.9)"
+        }
+    ]
+}
+
 function CFD() {
     return (
         <div>
@@ -11,7 +68,7 @@ function CFD() {
                 </div>
             </div>
             <div className="m-4">
-                Cummulative Flow Diagram
+                <Line options={options} data={data}></Line>
             </div>
         </div>
     );
