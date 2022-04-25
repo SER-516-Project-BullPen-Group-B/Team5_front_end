f4dc72a10b82dda1830da02da42f4382a158b613
varshik <sontivarshik@gmail.com>
2022-04-24 21:09:43 -0700

    Added funnel chart visualization for cycle time metric

--- src/pages/CycleTime.tsx
+++ src/pages/CycleTime.tsx
@@ -3,12 +3,14 @@ import DropDown from "../components/DropDown";
 import localForage from "localforage";
 import { useEffect, useState } from "react";
 import LineChart from "../components/LineChart";
+import { FunnelChart } from 'react-funnel-pipeline'
+import 'react-funnel-pipeline/dist/index.css'
 
 const CycleTime: React.FC = () => {
   const [data, setData] = useState("");
-  const [select, setSelect] = useState("Bar");
-  const types = ["Bar", "Line"];
-
+  const [select, setSelect] = useState("Funnel");
+  const types = ["Bar", "Line", "Funnel"];
+  const [funnelData, setFunnelData] = useState([] as any);
   useEffect(() => {
     /* eslint-disable  @typescript-eslint/no-explicit-any */
     localForage.getItem("cycleTime", (err, value: any) => {
@@ -23,19 +25,33 @@ const CycleTime: React.FC = () => {
           {
             label: "Cycle Time",
             data: Object.values(value),
-            backgroundColor: color,
+            backgroundColor: ["rgba(75, 0, 130, 0.7)"],
             borderWidth: 3,
           },
         ],
       };
-      setData(JSON.stringify(data));
+      let FunnelData: any[] = [];
+      if(select === "Funnel"){
+        for(let i = 0;i<Object.values(value).length;i++){
+          let item:any = {};
+          item = {
+            "name" : Object.keys(value)[i],
+            "value" : Object.values(value)[i],
+          }
+          FunnelData = [item, ...FunnelData]
+        };
+      };
+      FunnelData.sort(function(a, b) { 
+        return a.value - b.value;
+      });
+      FunnelData = FunnelData.reverse();
+      select==="Funnel"?setFunnelData(FunnelData):setData(JSON.stringify(data));
     });
   }, [select]);
-
   return (
     <div>
       <div className="flex justify-end ...">
-        <div className="m-6">
+        <div className="m-6 pb-10">
           {" "}
           <DropDown values={types} select={setSelect} title={select} />
         </div>
@@ -54,6 +70,19 @@ const CycleTime: React.FC = () => {
         {select === "Line" ? (
           data ? (
             <LineChart data={data} />
+          ) : (
+            <div>
+              Data not available, please make a valid request before you visit
+              this page.
+            </div>
+          )
+        ) : null}
+        {select === "Funnel" ? (
+          funnelData ? (
+            <div className="pt-10 mt-10">
+              <FunnelChart 
+              data={funnelData} />
+            </div>
           ) : (
             <div>
               Data not available, please make a valid request before you visit
