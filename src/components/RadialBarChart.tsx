1f3d3bba0826cf35607b6d8e3f09c9b1e25b23df
varshik <sontivarshik@gmail.com>
2022-04-26 17:33:56 -0700

    Adding Radial Bar Chart to visualize throughput

---
+++ src/components/RadialBarChart.tsx
@@ -0,0 +1,46 @@
+import { ResponsiveRadialBar } from '@nivo/radial-bar'
+
+const MyResponsiveRadialBar = ({ data /* see data tab */ }) => {
+    return (
+        <div style={{height: 700}}>
+        <ResponsiveRadialBar
+            data={data}
+            valueFormat=">-.2f"
+            padding={0.4}
+            maxValue = {100}
+            colors = {{ scheme:'set1' }}
+            cornerRadius={2}
+            endAngle ={360}
+            margin={{ top: 40, right: 120, bottom: 40, left: 40 }}
+            radialAxisStart={{ tickSize: 5, tickPadding: 5, tickRotation: 0 }}
+            circularAxisOuter={{ tickSize: 5, tickPadding: 12, tickRotation: 0 }}
+            legends={[
+                {
+                    anchor: 'right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 80,
+                    translateY: 0,
+                    itemsSpacing: 6,
+                    itemDirection: 'left-to-right',
+                    itemWidth: 100,
+                    itemHeight: 18,
+                    itemTextColor: '#999',
+                    symbolSize: 18,
+                    symbolShape: 'square',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemTextColor: '#000'
+                            }
+                        }
+                    ]
+                }
+            ]}
+        />
+        </div>
+    )
+}
+
+export default MyResponsiveRadialBar;
