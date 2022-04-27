6cc5c60449f67142b9aaed83491e8389a0043958
varshik <sontivarshik@gmail.com>
2022-04-26 20:09:59 -0700

    Integrating the bullet chart to page and visualizing the chart using sample data

--- src/components/BulletChart.tsx
+++ src/components/BulletChart.tsx
@@ -1,15 +1,22 @@
 import { ResponsiveBullet } from '@nivo/bullet'
 
 
-const MyResponsiveBullet = ({ data /* see data tab */ }) => (
-    <ResponsiveBullet
-        data={data}
-        margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
-        spacing={46}
-        titleAlign="start"
-        titleOffsetX={-70}
-        measureSize={0.2}
-    />
-)
+const BulletChart = ({ data }) => {
+   return (
+        <div style={{height: 500}} className="mt-10">
+            <ResponsiveBullet
+                data={data}
+                margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
+                spacing={46}
+                rangeColors = {'seq:blues'}
+                measureColors = {'seq:red_yellow_green'}
+                markerColors = {'seq:red_yellow_green'}
+                titleAlign="start"
+                titleOffsetX={-70}
+                measureSize={0.2}
+            />
+        </div>
+   )
+}
 
-export default MyResponsiveBullet;
+export default BulletChart;
--- src/pages/Throughput.tsx
+++ src/pages/Throughput.tsx
@@ -1,12 +1,92 @@
 import DropDown from "../components/DropDown";
 import { useState } from "react";
 import RadialBarChart from "../components/RadialBarChart";
+import BulletChart from "../components/BulletChart";
 
 const Throughput: React.FC = () => {
-  const [select, setSelect] = useState("Radial Bar");
-  const types = ["Radial Bar"];
+  const [select, setSelect] = useState("Bullet");
+  const types = ["Bullet","RadialBar"];
 
-  const data = 
+  const bulletData = 
+  [
+    {
+      "id": "Sprint 1",
+      "ranges": [
+        0,
+        40,
+        70,
+        100
+      ],
+      "measures": [
+        9
+      ],
+      "markers": [
+        9
+      ]
+    },
+    {
+      "id": "Sprint 2",
+      "ranges": [
+        0,
+        40,
+        70,
+        100
+      ],
+      "measures": [
+        22
+      ],
+      "markers": [
+        22
+      ]
+    },
+    {
+      "id": "Sprint 3",
+      "ranges": [
+        0,
+        40,
+        70,
+        100
+      ],
+      "measures": [
+        57
+      ],
+      "markers": [
+        57
+      ]
+    },
+    {
+      "id": "Sprint 4",
+      "ranges": [
+        0,
+        40,
+        70,
+        100
+      ],
+      "measures": [
+        89
+      ],
+      "markers": [
+        89
+      ]
+    },
+    {
+      "id": "Sprint 5",
+      "ranges": [
+        0,
+        40,
+        70,
+        100
+      ],
+      "measures": [
+        100
+      ],
+      "markers": [
+        100
+      ]
+    }
+  ]
+
+  const radialData = 
     [
         {
           "id": "1",
@@ -36,20 +116,29 @@ const Throughput: React.FC = () => {
           ]
         }
       ]
-
+  
   return (
     <div>
       <div className="flex justify-end ...">
-        <div className="m-6">
+        <div className="m-6 mb-10">
           {" "}
           <DropDown values={types} select={setSelect} title={select} />
         </div>
       </div>
       <div className="m-4">
-        
-        {select === "Radial Bar" ? (
-          data ? (
-            <RadialBarChart data={data} />
+        {select === "RadialBar" ? (
+          radialData ? (
+            <RadialBarChart data={radialData} />
+          ) : (
+            <div>
+              Data not available, please make a valid request before you visit
+              this page.
+            </div>
+          )
+        ) : null}
+        {select === "Bullet" ? (
+          bulletData ? (
+            <BulletChart data={bulletData} />
           ) : (
             <div>
               Data not available, please make a valid request before you visit
