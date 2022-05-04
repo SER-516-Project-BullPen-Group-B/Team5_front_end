f19d49728df9027506a016757fcba8195e25f3cd
achuthrajula <achuthrajula1999@gmail.com>
2022-05-03 20:43:45 -0700

    Addressed dependency issue raised by netlify

--- src/pages/Aworkspread.tsx
+++ src/pages/Aworkspread.tsx
@@ -1,4 +1,3 @@
-import BarChart from "../components/BarChart";
 import DropDown from "../components/DropDown";
 import localForage from "localforage";
 import { useEffect, useState } from "react";
@@ -8,16 +7,25 @@ import BasicTable from "../components/Acceptedworkspread";
 const Aworkspread: React.FC = () => {
   const [data, setData] = useState("");
   const [select, setSelect] = useState("Bar");
-  const types = ["Bar", "Line", "Radar", "PolarArea", "Pie", "Doughnut","Table"];
-  
+  const types = [
+    "Bar",
+    "Line",
+    "Radar",
+    "PolarArea",
+    "Pie",
+    "Doughnut",
+    "Table",
+  ];
+
   console.log(select);
   useEffect(() => {
     /* eslint-disable  @typescript-eslint/no-explicit-any */
     localForage.getItem("cycleTime", (err, value: any) => {
       const labels = Object.keys(value);
-      const color = (select==="Line")?[
-        'rgba(75, 0, 130, 0.7)',
-      ]:["rgba(07, 90, 128, 0.6)"];    
+      const color =
+        select === "Line"
+          ? ["rgba(75, 0, 130, 0.7)"]
+          : ["rgba(07, 90, 128, 0.6)"];
       const data = {
         labels,
         datasets: [
@@ -42,9 +50,14 @@ const Aworkspread: React.FC = () => {
         </div>
       </div>
       <div className="m-4">
-        {select === "Bar" ? data ? <BarChart data={data} /> : <div>Data not available ...</div>:null}
-        {select === "Line" ? data ? <LineChart data={data} /> : <div>Data not available ...</div>:null}
-        {select === "Table" ?<BasicTable />  :null}
+        {select === "Line" ? (
+          data ? (
+            <LineChart data={data} />
+          ) : (
+            <div>Data not available ...</div>
+          )
+        ) : null}
+        {select === "Table" ? <BasicTable /> : null}
       </div>
     </div>
   );
