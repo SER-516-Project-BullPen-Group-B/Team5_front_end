d9fc28112b5020973f218105088e8cd633d4e97b
achuthrajula <achuthrajula1999@gmail.com>
2022-05-04 18:36:11 -0700

    Mapping gauge meter to received respose data

--- src/components/Gauge.tsx
+++ src/components/Gauge.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Chart } from "react-google-charts";
+import PropTypes from "prop-types";
 
 function getRandomNumber() {
   return Math.random() * 100;
@@ -8,9 +9,7 @@ function getRandomNumber() {
 export function getData() {
   return [
     ["Label", "Value"],
-    ["Memory", getRandomNumber()],
-    ["CPU", getRandomNumber()],
-    ["Network", getRandomNumber()],
+    ["Scope Change", getRandomNumber()],
   ];
 }
 
@@ -21,29 +20,44 @@ export const options = {
   redTo: 100,
   yellowFrom: 75,
   yellowTo: 90,
+  lightgreenFrom: 30,
+  lightgreenTo: 55,
   minorTicks: 5,
+  animation: {
+    duration: 1000,
+    easing: "out",
+  },
 };
 
-export default function Gauge() {
-  const [data, setData] = useState(getData);
+interface GaugeProps {
+  value: string;
+}
+const Gauge: React.FC<GaugeProps> = ({ value }) => {
+  const [metric, setMetric]: any[] = useState([
+    ["Label", "Value"],
+    ["Scope Change", 0],
+  ]);
 
   useEffect(() => {
+    const gaugeValue = [
+      ["Label", "Value"],
+      ["Scope Change", JSON.parse(value)],
+    ];
     const id = setInterval(() => {
-      setData(getData());
-    }, 3000);
-
+      setMetric(gaugeValue);
+    }, 1000);
     return () => {
       clearInterval(id);
     };
   });
 
   return (
-    <Chart
-      chartType="Gauge"
-      width="100%"
-      height="400px"
-      data={data}
-      options={options}
-    />
+    <Chart chartType="Gauge" width="100%" data={metric} options={options} />
   );
-}
+};
+
+Gauge.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
+export default Gauge;
